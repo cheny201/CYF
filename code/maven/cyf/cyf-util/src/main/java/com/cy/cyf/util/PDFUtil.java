f7d8c0ff179ea221138e94ebb059a60f5f3ba2aa
ChenY201 <cheny201@163.com>
2015-07-04 21:50:28 +0800

    添加jar包

--- code/maven/cyf/cyf-util/src/main/java/com/cy/cyf/util/PDFUtil.java
+++ code/maven/cyf/cyf-util/src/main/java/com/cy/cyf/util/PDFUtil.java
@@ -1,31 +1,31 @@
 package com.cy.cyf.util;
 
-import java.awt.Image;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.icepdf.core.exceptions.PDFException;
+import org.icepdf.core.exceptions.PDFSecurityException;
+import org.icepdf.core.pobjects.Document;
+import org.icepdf.core.pobjects.Page;
+import org.icepdf.core.util.GraphicsRenderingHints;
 
 import com.cy.cyf.log.CYFLog;
-import com.sun.pdfview.PDFFile;
-import com.sun.pdfview.PDFPage;
 
 public class PDFUtil {
 
 	public static void main(String[] args) throws Exception {
-		String filePath = "f:/11/Maven实战.pdf";
+		String filePath = "f:/11/lcpoldata_20150601_08_21_47_86110020150210045039.pdf";
 		String outDir = "f:/11/picture";
-//		PDFUtil.convertToJPGByPDFBox(filePath, outDir);
-		PDFUtil.convertToJPGByPDFRender(filePath, outDir);
+		// PDFUtil.convertToJPGByPDFBox(filePath, outDir);
+		PDFUtil.convertToJPGByIcePDF(filePath, outDir);
+
 	}
 
 	/**
@@ -54,47 +54,44 @@ public class PDFUtil {
 		CYFLog.debug("[" + filePath + "]转换完成，耗时[" + (end - start) + "]ms");
 	}
 
+
 	/**
-	 * 将pdf转换成jpg，中文文件会有问题
+	 * 将pdf转换成jpg
 	 * 
 	 * @param filePath
 	 *            pdf路径
 	 * @param outDir
 	 *            输出图片目录
 	 * @throws IOException
+	 * @throws PDFSecurityException
+	 * @throws PDFException
 	 */
-	public static void convertToJPGByPDFRender(String filePath, String outDir) throws IOException {
+	public static void convertToJPGByIcePDF(String filePath, String outDir)
+			throws IOException, PDFException, PDFSecurityException {
 		long start = System.currentTimeMillis();
-		// load a PDF from a byte buffer
-		RandomAccessFile raf = new RandomAccessFile(filePath, "r");
-		FileChannel channel = raf.getChannel();
-		ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0,
-				channel.size());
-		raf.close();
-		PDFFile pdffile = new PDFFile(buf);
-		CYFLog.debug("页数： " + pdffile.getNumPages());
-		for (int i = 1; i <= pdffile.getNumPages(); i++) {
-			// draw the first page to an image
-			PDFPage page = pdffile.getPage(i);
 
-			// get the width and height for the doc at the default zoom
-			Rectangle rect = new Rectangle(0, 0, (int) page.getBBox()
-					.getWidth(), (int) page.getBBox().getHeight());
-			System.out.println(i+"==============="+page.getBBox().getHeight());
-			// generate the image
-			Image img = page.getImage(rect.width, rect.height, // width & height
-					rect, // clip rect
-					null, // null for the ImageObserver
-					true, // fill background with white
-					true // block until drawing is done
-					);
-			BufferedImage tag = new BufferedImage(rect.width, rect.height,
-					BufferedImage.TYPE_INT_RGB);
-			tag.getGraphics().drawImage(img, 0, 0, rect.width, rect.height,
-					null);
-			ImageIO.write(tag, "jpg", new File(outDir + File.separator + i
-					+ ".jpg"));
+		Document document = new Document();
+		document.setFile(filePath);
+
+		// save page caputres to file.
+		float scale = 3f;
+		float rotation = 0f;
+
+		// Paint each pages content to an image and write the image to file
+		for (int i = 0; i < document.getNumberOfPages(); i++) {
+			BufferedImage image = (BufferedImage) document.getPageImage(i,
+					GraphicsRenderingHints.SCREEN, Page.BOUNDARY_CROPBOX,
+					rotation, scale);
+			RenderedImage rendImage = image;
+			// capture the page image to file
+			CYFLog.debug("\t capturing page " + i);
+			ImageIO.write(rendImage, "jpg", new File(outDir + File.separator
+					+ i + ".jpg"));
+			image.flush();
 		}
+		// clean up resources
+		document.dispose();
+
 		long end = System.currentTimeMillis();
 		CYFLog.debug("[" + filePath + "]转换完成，耗时[" + (end - start) + "]ms");
 	}
